993a6632f5dd08f4935acf63d424c7705cd4d2b2
farjar <24551292+farisjarrah@users.noreply.github.com>
2020-09-14 19:10:51 -0700

    Unfinished project euler number 3

---
+++ python/src/project-euler/3.py
@@ -0,0 +1,36 @@
+num = 228
+step = num
+last_prime = 2
+next_prime = 2
+prime_factors = []
+
+if step % last_prime == 0:
+    prime_factors.append(last_prime)
+    next_prime = last_prime
+
+
+
+
+def find_prime(n):
+    solved = False
+    target_prime = 0
+    last_prime = n
+    while solved == False:
+        target_prime = last_prime + 1
+            if target_prime % i == 0:
+                pass
+        else:
+            if i
+
+
+def factorization(n):
+    num = n
+    factor = 2
+    factors_list = []
+    while factor != num:
+        if num % factor == 0:
+            factors_list.append(2)
+        elif num % factor != 0:
+            find_next = 0
+
+            factor = factor + 1
